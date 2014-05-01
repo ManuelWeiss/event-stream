b2639576234e531fbd40cb77c30f8424a604847c
Francesco Zaia <fzaia@moonfruit.com>
2014-05-01 11:45:39 +0100

    introduced d3.js

--- app/controllers/ChatApplication.scala
+++ app/controllers/ChatApplication.scala
@@ -19,7 +19,7 @@ object ChatApplication extends Controller {
   def indexReact = Action { Ok(views.html.react("Chat using Server Sent Events and React")) }
 
   /** Controller action serving Francesco chat page */
-    def indexFrancesco = Action { Ok(views.html.francesco("Chat using Server Sent Events and Francesco")) }
+  def indexFrancesco = Action { Ok(views.html.francesco("Moonfruit Data Visualization")) }
 
   /** Controller action for POSTing chat messages */
   def postMessage = Action(parse.json) { req => chatChannel.push(req.body); Ok }
