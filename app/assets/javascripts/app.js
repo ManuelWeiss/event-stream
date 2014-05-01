879ab16ac3fd6b521312da1d727751f9f4c4c5dc
Francesco Zaia <fzaia@moonfruit.com>
2014-05-01 12:37:43 +0100

    introduced d3.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,6 @@
 'use strict';
 
 /** app level module which depends on services and controllers */
-angular.module('sseChat', ['sseChat.services', 'sseChat.controllers', 'd3.visualization']);
+angular.module('sseChat', ['sseChat.services', 'sseChat.controllers']);
+
+angular.module('dataVisualization', ['sseChat', 'd3.visualization']);
