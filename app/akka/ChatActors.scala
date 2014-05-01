75a3f973fc7fd705ba4e21a785a691d2098413d0
Claudia Matosa <cmatosa@moonfruit.com>
2014-05-01 14:29:38 +0100

    Adding page information to mocked data

--- app/akka/ChatActors.scala
+++ app/akka/ChatActors.scala
@@ -27,7 +27,7 @@ object ChatActors {
 class Supervisor() extends Actor {
 
   val events = context.actorOf(Props(new Chatter(UserEvents.events)))
-  context.system.scheduler.schedule(1 seconds, 2 seconds, events, ChatActors.Talk)
+  context.system.scheduler.schedule(1 seconds, 0.2 seconds, events, ChatActors.Talk)
 
   def receive = { case _ => }
 }
@@ -48,6 +48,11 @@ object UserEvents {
   val events = Seq(
     Json.obj(
         "stream" -> "registration",
+        "page" -> Json.obj (
+            "category" -> "Dashboard",
+            "path" -> "/",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 34242423,
           "language" -> "en-GB",
@@ -57,6 +62,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "registration",
+        "page" -> Json.obj (
+            "category" -> "404",
+            "path" -> "/signup/register",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 4564564,
           "language" -> "en-GB",
@@ -66,6 +76,39 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "registration",
+        "page" -> Json.obj (
+            "category" -> "Pricing",
+            "path" -> "/pricing",
+            "environment" -> "development"
+        ),
+        "user" -> Json.obj (
+          "userId" -> 34534535,
+          "language" -> "en-GB",
+          "returning" -> false,
+          "price_id" -> "SMT"
+        )
+    ),
+    Json.obj(
+        "stream" -> "registration",
+        "page" -> Json.obj (
+            "category" -> "Dashboard",
+            "path" -> "/home",
+            "environment" -> "development"
+        ),
+        "user" -> Json.obj (
+          "userId" -> 34534535,
+          "language" -> "en-GB",
+          "returning" -> false,
+          "price_id" -> "SMT"
+        )
+    ),
+    Json.obj(
+        "stream" -> "registration",
+        "page" -> Json.obj (
+            "category" -> "About",
+            "path" -> "/about",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 34534535,
           "language" -> "en-GB",
@@ -75,6 +118,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "login",
+        "page" -> Json.obj (
+            "category" -> "Home",
+            "path" -> "/",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 6456456,
           "language" -> "en-US",
@@ -84,6 +132,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "login",
+        "page" -> Json.obj (
+            "category" -> "Pricing",
+            "path" -> "/pricing",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 3453453,
           "language" -> "en-US",
@@ -93,6 +146,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "login",
+        "page" -> Json.obj (
+            "category" -> "Home",
+            "path" -> "/",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 5676422,
           "language" -> "en-US",
@@ -102,6 +160,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "site-creation",
+        "page" -> Json.obj (
+            "category" -> "Home",
+            "path" -> "/",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 5676422,
           "language" -> "en-US",
@@ -111,6 +174,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "site-creation",
+        "page" -> Json.obj (
+            "category" -> "Home",
+            "path" -> "/",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 34534535,
           "language" -> "en-US",
@@ -120,6 +188,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "site-creation",
+        "page" -> Json.obj (
+            "category" -> "Home",
+            "path" -> "/",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 34242423,
           "language" -> "en-US",
@@ -129,6 +202,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "site-creation",
+        "page" -> Json.obj (
+            "category" -> "Home",
+            "path" -> "/",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 4546456,
           "language" -> "en-US",
@@ -138,6 +216,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "site-creation",
+        "page" -> Json.obj (
+            "category" -> "Home",
+            "path" -> "/",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 34534534,
           "language" -> "en-US",
@@ -147,6 +230,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "subscription",
+        "page" -> Json.obj (
+            "category" -> "Pricing",
+            "path" -> "/pricing",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 5676422,
           "language" -> "en-US",
@@ -156,6 +244,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "subscription",
+        "page" -> Json.obj (
+            "category" -> "Pricing",
+            "path" -> "/pricing",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 34534535,
           "language" -> "en-US",
@@ -165,6 +258,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "subscription",
+        "page" -> Json.obj (
+            "category" -> "Pricing",
+            "path" -> "/pricing",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 34242423,
           "language" -> "en-US",
@@ -174,6 +272,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "subscription",
+        "page" -> Json.obj (
+            "category" -> "Kickstart Home",
+            "path" -> "/kickstart",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 4546456,
           "language" -> "en-US",
@@ -183,6 +286,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "subscription",
+        "page" -> Json.obj (
+            "category" -> "Kickstart Home",
+            "path" -> "/kickstart",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 6456456,
           "language" -> "en-US",
@@ -192,6 +300,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "cancellation",
+        "page" -> Json.obj (
+            "category" -> "Billing",
+            "path" -> "/billing",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 5676422,
           "language" -> "en-US",
@@ -201,6 +314,11 @@ object UserEvents {
     ),
     Json.obj(
         "stream" -> "cancellation",
+        "page" -> Json.obj (
+            "category" -> "Billing",
+            "path" -> "/billing",
+            "environment" -> "development"
+        ),
         "user" -> Json.obj (
           "userId" -> 353453,
           "language" -> "en-US",
